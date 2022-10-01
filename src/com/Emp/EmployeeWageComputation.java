3a595d853bf3cdad5825b497801fc04fc906a688
shadab <me.shadab19@gmail.com>
2022-10-01 12:41:10 +0530

    [Added: Welcome Massage]

---
+++ src/com/Emp/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com.Emp;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcom To Employee Wage Computation Problem");
+
+	}
+
+}
