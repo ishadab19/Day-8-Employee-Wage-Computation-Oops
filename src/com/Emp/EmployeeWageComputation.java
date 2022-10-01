0a6eaa7087491f47b0829b023f2489015f821bb1
shadab <me.shadab19@gmail.com>
2022-10-01 13:08:11 +0530

    [Added: Getting Monthly Salary Using Switch case]

--- src/com/Emp/EmployeeWageComputation.java
+++ src/com/Emp/EmployeeWageComputation.java
@@ -8,32 +8,41 @@ public class EmployeeWageComputation {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
+
 		Random random = new Random();
 
 		int wagePerHour = 20;
 		int fullDayHours = 8;
 		int halfDayHours = 4;
 		int salary = 0;
+		int monthlysalary = 0;
+		int workingDaysPerMonth = 20;
+		
+		for(int i = 1; i <= workingDaysPerMonth; i++) {
 		int randomNum = random.nextInt(3);
 		
 		switch(randomNum) {
 		
 		case 0:
 			System.out.println("Employee is absent");
-			System.out.println("Monthly Salary wil be:~ ");
+			System.out.println("Salary wil be:~ ");
 			break;
 		case 1:
 			System.out.println("Employee is Present");
-			System.out.println("Monthly salary for Full Day :~ ");
+			System.out.println("salary for Full Day :~ ");
 			salary = wagePerHour * fullDayHours;
 			break;
 		case 2:
 			System.out.println("Employee is present");
-			System.out.println("Monthly Salary for Half Day:~ ");
+			System.out.println("Salary for Half Day:~ ");
 			salary = wagePerHour * halfDayHours;
 			break;
 		}
+			monthlysalary = monthlysalary + salary;
 			System.out.println(+salary);
+			
+		}
+		System.out.println("Monthly Salary:~"+monthlysalary);
 			
 	}
 
