04fdc30c42e5f26aad36619414b1cc385a69fc09
shadab <me.shadab19@gmail.com>
2022-10-01 12:46:35 +0530

    [Added: checking Emp is absent Or present]

--- src/com/Emp/EmployeeWageComputation.java
+++ src/com/Emp/EmployeeWageComputation.java
@@ -1,9 +1,20 @@
 package com.Emp;
 
+import java.util.*;
+
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		System.out.println("Welcom To Employee Wage Computation Problem");
+
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+		Random random = new Random();
+		int randomNum = random.nextInt(2);
+
+		if (randomNum == 1)
+			System.out.println("Employee  is Present");
+		else
+			System.out.println("Employee  is Absent");
 
 	}
 
