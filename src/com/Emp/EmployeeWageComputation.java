32456cbdd63e041d23c5fbe8dea9374c1a6b706a
shadab <me.shadab19@gmail.com>
2022-10-01 12:56:28 +0530

    [Added:  Part Time Salary]

--- src/com/Emp/EmployeeWageComputation.java
+++ src/com/Emp/EmployeeWageComputation.java
@@ -11,14 +11,20 @@ public class EmployeeWageComputation {
 
 		int wagePerHour = 20;
 		int fullDayHours = 8;
+		int halfDayHours = 4;
 		int salary = 0;
-		int randomNum = random.nextInt(2);
+		int randomNum = random.nextInt(3);
 		if (randomNum == 1) {
 			System.out.println("Employee  is Present");
 			salary = wagePerHour * fullDayHours;
+			System.out.println("Monthly salary for Full day is: ");
+		} else if (randomNum == 2) {
+			salary = halfDayHours * wagePerHour;
+
+			System.out.println("Monthly salary for Half day is: ");
 		} else
 			System.out.println("Employee  is Absent");
-		System.out.println("salary is: "+salary);
+		System.out.println("salary is: " + salary);
 	}
 
 }
