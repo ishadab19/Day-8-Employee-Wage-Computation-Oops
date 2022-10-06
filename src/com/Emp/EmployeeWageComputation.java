e348d3efe0ef8e89ee3f462602bb3d6dc8142696
shadab <me.shadab19@gmail.com>
2022-10-06 19:29:08 +0530

    [Added- ability to save Wage for each Company]

--- src/com/Emp/EmployeeWageComputation.java
+++ src/com/Emp/EmployeeWageComputation.java
@@ -7,65 +7,72 @@ public class EmployeeWageComputation {
 	public static final int fullDayHours = 8;
 
 	private final String companyName;
-	private final int WagePerHour;
+	private final int empWagePerHour;
 	private final int workingHours;
 	private final int workingDays;
-	
+	int totalSalary;
 	static Random random = new Random();
 
-	public EmployeeWageComputation(String companyName, int wagePerHour, int workingHours, int workingDays) {
+	public EmployeeWageComputation(String companyName, int empWagePerHour, int workingHours, int workingDays) {
 		this.companyName = companyName;
-		this.WagePerHour = wagePerHour;
+		this.empWagePerHour = empWagePerHour;
 		this.workingDays = workingDays;
 		this.workingHours = workingHours;
-
 	}
 
-	public int calculateWage() {
+	public void calculateWage() {
 		int salary = 0;
-		int totalSalary = 0;
+		totalSalary = 0;
 		int totalWorkingHours = 0;
 		int days = 0;
 
 		while (totalWorkingHours <= workingHours && days <= workingDays) {
 			days++;
+
 			int empCheck = random.nextInt(3);
 
 			switch (empCheck) {
 			case 1:
-
-				System.out.println("Present Fullday.");
-				salary = WagePerHour * fullDayHours;
-				totalWorkingHours = totalWorkingHours + fullDayHours;
+				
+				 System.out.println("Present Fullday.");
+				 salary = empWagePerHour * fullDayHours;
+				 totalWorkingHours = totalWorkingHours + fullDayHours;
 				break;
 			case 2:
-
-				System.out.println("Present HalfDay.");
-				salary = WagePerHour * halfDayHours;
-				totalWorkingHours = totalWorkingHours + halfDayHours;
+				
+				 System.out.println("Present HalfDay.");
+				 salary = empWagePerHour * halfDayHours;
+				 totalWorkingHours = totalWorkingHours + halfDayHours;
 				break;
 			default:
 				System.out.println("Employee Absent");
 			}
-			System.out.print("Day: " + days  + empCheck + " ");
-			System.out.print("Working Hours: " + totalWorkingHours + " ");
-			System.out.print("Salary is: " + salary + " ");
+			
+			System.out.print("Day: "+days+"\t  "+empCheck+"\t");
+			System.out.print("Working Hours: "+totalWorkingHours+"\t");
+			System.out.print("Salary is: "+salary+"\t");
 			totalSalary = totalSalary + salary;
+			
 		}
-		return totalWorkingHours * WagePerHour;
-
+		
+		//return totalWorkingHours * empWagePerHour;
 	}
-
+	public String toString() {
+		return "Total Wage for Company: "+companyName+" is "+totalSalary;
+		
+	}
 
 	public static void main(String[] args) {
-		EmployeeWageComputation TCS = new EmployeeWageComputation("TCS", 23, 90, 21);
-		EmployeeWageComputation Honda = new EmployeeWageComputation("Honda", 24, 109, 22);
 		
-		System.out.println("Total employee's wage of Company: " + TCS.companyName + ": " + TCS.calculateWage());
-		 System.out.println("Total employee's wage of Company: " + Honda.companyName + ": " + Honda.calculateWage());
+		 EmployeeWageComputation TCS = new EmployeeWageComputation("TCS", 23, 90, 21);
+		 EmployeeWageComputation Honda = new EmployeeWageComputation("Honda", 24, 109, 22);
+		 TCS.calculateWage();
+		 System.out.println(TCS);
+		 Honda.calculateWage();
+		 System.out.println(Honda);
+		 
 		
 		
 	}
-
 
 	}
